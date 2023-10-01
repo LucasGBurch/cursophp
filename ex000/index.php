7231ac5cdf3757e79d00090e49c33510f17dfec7
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-10-01 12:51:27 -0300

    Primeiros programas
    Primeiros códigos com servidor PHP no XAMPP. Mostrando como o php se acomoda com HTML em um código da página de servidor localhost da apache.

---
+++ ex000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Me livrando da Maldição</title>
+</head>
+<body>
+  <h1>
+    <?php 
+      echo "\u{1F30E} Olá, Mundo! 🌎"; // pode ser print no lugar de echo
+      // Emojipedia hoje em dia dá pra copiar direto o emoji, mas dá pra introduzir pelo código unicode \u{unicode}
+    ?>
+  </h1>
+  <p>Vamos tentar nos livrar da maldição</p>
+</body>
+</html>
