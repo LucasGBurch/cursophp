da652d07dde8f7e91a51598334f66b41ba521c69
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-10-05 17:18:07 -0300

    Desafio 03 - Conversor básico
    Conversor de moeda que já faz uso de conversão de formato de dados. Destaque para função numfmt para padronizar a moeda através da conhecida biblioteca INTL. O avançado será o próximo, feito com API do Banco Central.

--- modulo1/desafios/desafio002/index.php
+++ modulo1/desafios/desafio002/index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../../exercicios/ex004/style.css">
-  <title>Desafio Antecessor e Sucessor</title>
+  <title>Desafio Número Aleatório</title>
 </head>
 
 <body>
--- modulo1/desafios/desafio003/solucao.php
+++ modulo1/desafios/desafio003/solucao.php
@@ -1,12 +1,31 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../../exercicios/ex004/style.css">
-  <title>Desafio Antecessor e Sucessor</title>
+  <title>Desafio Moeda Básico</title>
 </head>
+
 <body>
-  
+  <main>
+    <h1>Conversor de Moedas</h1>
+    <?php
+    // Padrão para moeda. Desabilitada em servidor local, por isso foi preciso habilitar no php.ini o extension=intl e reiniciar o servidor)
+    $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+    // Formatter da biblioteca INTL, aquela usada no Ignite também existe no PHP moderno!
+
+    $cotacao = 5.17;
+    $reais = $_GET["reais"] ?? 0;
+    $dolares = $reais / $cotacao;
+
+    // Modo simples de formatação: number_format($reais / $cotacao, 2, ",", ".");
+
+    echo "<p>Seus " . numfmt_format_currency($padrao, $reais, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrao, $dolares, "USD") . "</strong>.</p>"
+    ?>
+    <button onclick="javascript:history.go(-1)">&larr; Voltar</button>
+  </main>
 </body>
+
 </html>
